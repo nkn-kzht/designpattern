232a8a4ec20fad3204e6e3371ef3d9e7260ff799
kazuhito <kazuhito@localhost.localdomain>
2015-08-20 02:49:55 +0900

    不要なプロジェクト（System）を削除

--- System/Release/sources/main.d
+++
@@ -1,7 +0,0 @@
-sources/main.d: ../sources/main.cpp \
- /home/parallels/workspace/System/includes/SystemImpl.h \
- /home/parallels/workspace/System/includes/ISystem.h
-
-/home/parallels/workspace/System/includes/SystemImpl.h:
-
-/home/parallels/workspace/System/includes/ISystem.h:
